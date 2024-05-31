0475a2b668b6cba4fee97e8dc9ccd24469e663a9
Aliaksei Yafremau <447654972g@gmail.com>
2024-05-31 17:32:39 +0200

    Add Article

--- HealthyLifeStyle/health_app/serializers.py
+++ HealthyLifeStyle/health_app/serializers.py
@@ -26,3 +26,9 @@ class AllergySerializer(serializers.ModelSerializer):
     class Meta:
         model = Allergy
         fields = ['name']
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['author', 'date_created', 'text']
