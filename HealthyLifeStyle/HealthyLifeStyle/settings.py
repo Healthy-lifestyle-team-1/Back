b8729514c2cecea235516acae5e425c01402aba7
dnszhkv <den1szhuk0v@yandex.ru>
2024-06-18 22:31:47 +0700

    Update settings.py

--- HealthyLifeStyle/HealthyLifeStyle/settings.py
+++ HealthyLifeStyle/HealthyLifeStyle/settings.py
@@ -76,6 +76,8 @@ AUTH_USER_MODEL = 'user_app.User'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
